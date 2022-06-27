a10b09cedbe631594a1c4a2a056419a2ff97508e
Bryan Sandí <37526395+bryansandibarrantes@users.noreply.github.com>
2022-06-26 18:27:14 -0600

    Se agrega código final de las vistas. Se comenta el código

--- MARDA/pantallajuego.cpp
+++ MARDA/pantallajuego.cpp
@@ -9,96 +9,12 @@ PantallaJuego::PantallaJuego(QWidget *parent) :
     ui(new Ui::PantallaJuego)
 {
     ui->setupUi(this);
-    mazo mazoBryan;
-    std::string cartaSacadaDeMazo;
-    //cartaSacadaDeMazo = mazoBryan.obtenerCartaJugadorUno();
-    //std::cout << "Llega aca: " << cartaSacadaDeMazo << std::endl;
-
-    //QString carta = QString::fromStdString(cartaSacadaDeMazo);
-    //this->ui->pushButton_9->setText("carta");
-    mazoBryan.IniciarCartas();
-
-    /*QPixmap pixmap(":/img/img/ace_of_clubs.png");
-    this->ui->label_1->setPixmap(pixmap);
-    this->ui->label_1->setMask(pixmap.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_1->show();*/
-
-    /*QPixmap pixmap1("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/2_of_diamonds");
-    //QPixmap pixmap1("../PNG-cards-1.3/2_of_diamonds");
-    this->ui->label_2->setPixmap(pixmap1);
-    this->ui->label_2->setMask(pixmap1.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_2->show();
-
-    QPixmap pixmap2("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/queen_of_spades2");
-    //QPixmap pixmap2("../PNG-cards-1.3/queen_of_spades2");
-    this->ui->label_3->setPixmap(pixmap2);
-    this->ui->label_3->setMask(pixmap2.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_3->show();
-
-    QPixmap pixmap3("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/6_of_hearts");
-    //QPixmap pixmap3("../PNG-cards-1.3/6_of_hearts");
-    this->ui->label_4->setPixmap(pixmap3);
-    this->ui->label_4->setMask(pixmap3.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_4->show();
-
-    QPixmap pixmap4("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/3_of_diamonds");
-    //QPixmap pixmap4("../PNG-cards-1.3/3_of_diamonds");
-    this->ui->label_5->setPixmap(pixmap4);
-    this->ui->label_5->setMask(pixmap4.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_5->show();
-
-    //Segundo jugador
-    QPixmap pixmap5("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/ace_of_clubs");
-    //QPixmap pixmap5("../PNG-cards-1.3/ace_of_clubs");
-    this->ui->label_6->setPixmap(pixmap5);
-    this->ui->label_6->setMask(pixmap5.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_6->show();*/
-
-    QPixmap pixmap6("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/2_of_diamonds");
-    //QPixmap pixmap6("../PNG-cards-1.3/2_of_diamonds");
-    this->ui->label_7->setPixmap(pixmap6);
-    this->ui->label_7->setMask(pixmap6.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_7->show();
-
-    QPixmap pixmap7("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/queen_of_spades2");
-    //QPixmap pixmap7("../PNG-cards-1.3/queen_of_spades2");
-    this->ui->label_8->setPixmap(pixmap7);
-    this->ui->label_8->setMask(pixmap7.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_8->show();
-
-    QPixmap pixmap8("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/6_of_hearts");
-    //QPixmap pixmap8("../PNG-cards-1.3/6_of_hearts");
-    this->ui->label_9->setPixmap(pixmap8);
-    this->ui->label_9->setMask(pixmap8.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_9->show();
-
-    QPixmap pixmap9("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/3_of_diamonds");
-    //QPixmap pixmap9("../PNG-cards-1.3/3_of_diamonds");
-    this->ui->label_10->setPixmap(pixmap9);
-    this->ui->label_10->setMask(pixmap9.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_10->show();
-    //IniciarCartas();
-
-    //QPixmap pixmap12("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/8_of_clubs");
-    //QPixmap pixmap12("../PNG-cards-1.3/8_of_clubs");
-    /*this->ui->label_11->setPixmap(pixmap12);
-    this->ui->label_11->setMask(pixmap12.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_11->show();*/
-
-    /*QPixmap pixmap13("C:/Users/pc/Documents/SpiteAndMalice/PNG-cards-1.3/6_of_diamonds");
-    //QPixmap pixmap13("../PNG-cards-1.3/6_of_diamonds");
-    this->ui->label_14->setPixmap(pixmap13);
-    this->ui->label_14->setMask(pixmap13.mask());  //  C:\Users\andre\OneDrive\Escritorio\UCR\GitHub\SpiteAndMalice\PNG-cards-1.3
-    this->ui->label_14->show();*/
-    //IniciarCartas();
-
 }
+
 /*
 void PantallaJuego::IniciarCartas()
 {
-
     std::ifstream archivo("../Cartas.txt");
-
     std::string linea;
     // Obtener línea de archivo, y almacenar contenido en "linea"
     while (std::getline(archivo, linea)) {
@@ -108,7 +24,6 @@ void PantallaJuego::IniciarCartas()
 }
 */
 
-
 PantallaJuego::~PantallaJuego()
 {
     delete ui;
@@ -116,9 +31,6 @@ PantallaJuego::~PantallaJuego()
 
 void PantallaJuego::on_pushButton_2_clicked()
 {
-    this->ui->pushButton_2->setStyleSheet("image: url(:/img/img/7_of_clubs.png)");
-    //this->ui->label_5->setText("As de corazones");
-    //mazo *m = new mazo();
-    //m->CrearMazo()
-    //this->ui->label_5->setText("As de corazones");
+    this->ui->pushButton_2->setStyleSheet(
+                "image: url(:/img/img/7_of_clubs.png)");
 }
--- MARDA/pantallajuego.h
+++ MARDA/pantallajuego.h
@@ -14,12 +14,29 @@ class PantallaJuego : public QWidget
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor de la clase PantallaJuego donde se muestra y juega el
+     * juego.
+     * @param parent Parámetro de la clase padre QWidget
+     */
     explicit PantallaJuego(QWidget *parent = nullptr);
+
+    /**
+     * Destructor de la clase PantallaJuego
+     */
     ~PantallaJuego();
+
+    /**
+     * @brief Método para acceder a las imágenes de las cartas. De momento se
+     * encuentra inhabilitado
+     */
     void IniciarCartas();
 
 private slots:
-
+    /**
+     * @brief Realiza las acciones que se dicten dentro del código del método
+     * cuando el usuario le da clic a este botón en específico.
+     */
     void on_pushButton_2_clicked();
 
 private:
